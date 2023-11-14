fd2d1c66c41bdcbd737b862744e0523ef02350bc
Jeremy Frick <jeremy.frick@me.com>
2023-11-13 20:37:30 -0800

    Initial Commit

---
+++ JoJoBudgeting/JoJoBudgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoJoBudgetingApp.swift
+//  JoJoBudgeting
+//
+//  Created by Jeremy Frick on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct JoJoBudgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
